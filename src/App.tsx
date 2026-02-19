451fafa7a721a1038a41071864d9f8a6a51bdb86
dapperdile <rafinhawc@hotmail.com>
2026-02-19 16:06:59 -0300

    Criado a logica para consulta, criação, edição e remoção dos temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
+import FormTema from "./components/tema/formtema/FormTema";
+import ListaTemas from "./components/tema/listatemas/ListaTemas";
 import { AuthProvider } from "./contexts/AuthContext";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import DeletarTema from "./components/tema/deletartema/DeletarTema";
 
 function App() {
   return (
@@ -18,6 +21,10 @@ function App() {
               <Route path="/login" element={<Login />}></Route>
               <Route path="/home" element={<Home />}></Route>
               <Route path="/cadastro" element={<Cadastro />}></Route>
+              <Route path="/temas" element={<ListaTemas />}></Route>
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
